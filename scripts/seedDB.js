aed17a836d71dbb3ea8608cbc80e8d34dd3fd3a0
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-22 10:20:09 -0800

    add API routes to server.js and export models

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -22,3 +22,15 @@ const bookSeed = [
         title: "Harry Potter: The Complete Collection"
     }
 ];
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
