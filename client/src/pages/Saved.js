d5a503c72d09d3b7f7eb5e6a28ade4c162d32346
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-23 20:14:01 -0800

    change saved page to stateful component

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,34 +1,38 @@
-import React, { Fragment } from "react";
-import NavTabs from "./components/NavTabs/index";
-import Jumbotron from "./components/Jumbotron/index";
-import BookListItem from "./components/BookList/index";
-import BookList from "./components/BookList/index"
+import React, { Component, Fragment } from "react";
+import Jumbotron from "../components/Jumbotron";
+import BookListItem from "../components/BookListItem";
+import BookList from "../components/BookList"
 
-function Saved() {
-  return (
-    <Fragment>
-      <NavTabs />
-      <Jumbotron />
-      <div>
-        <h1>Saved Page</h1>
-      </div>
-      <BookList>
-        {this.state.books.map(book => {
-          return (
-            <BookListItem
-              key={book.label}
-              title={book.title}
-              link={book.url}
-              descripton={book.description}
-              image={book.image}
-              author={book.author}
-              page_type={this.state.PageType}
-            />
-          );
-        })}
-      </BookList>
-    </Fragment>
-  );
+class Saved extends Component {
+  state = {
+    savedBooks: []
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Jumbotron />
+        <div>
+          <h1>Saved Page</h1>
+        </div>
+        <BookList>
+          {this.state.savedBooks.map(book => {
+            return (
+              <BookListItem
+                key={book.label}
+                title={book.title}
+                link={book.url}
+                descripton={book.description}
+                image={book.image}
+                author={book.author}
+                page_type={this.state.PageType}
+              />
+            );
+          })}
+        </BookList>
+      </Fragment>
+    );
+  }
 }
 
 export default Saved;
