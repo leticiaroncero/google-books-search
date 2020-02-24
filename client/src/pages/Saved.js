bdb37afb9b5c9debc0eb3ac718c4d77cecd399ce
Matt DeMonaco <mdemonaco@gmail.com>
2020-02-23 19:31:21 -0800

    Coded saved.js file

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
+import NavTabs from "./components/NavTabs/index";
+import Jumbotron from "./components/Jumbotron/index";
+import BookListItem from "./components/BookList/index";
+import BookList from "./components/BookList/index"
 
 function Saved() {
   return (
-    <div>
-      <h1>Saved Page</h1>
-    </div>
+    <Fragment>
+      <NavTabs />
+      <Jumbotron />
+      <div>
+        <h1>Saved Page</h1>
+      </div>
+      <BookList>
+        {this.state.books.map(book => {
+          return (
+            <BookListItem
+              key={book.label}
+              title={book.title}
+              link={book.url}
+              descripton={book.description}
+              image={book.image}
+              author={book.author}
+              page_type={this.state.PageType}
+            />
+          );
+        })}
+      </BookList>
+    </Fragment>
   );
 }
 
